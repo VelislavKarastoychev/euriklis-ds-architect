05cb3a596c56d520d31561cbdd4fd2740ea7d4f3
euriklis <exel_mmm@abv.bg>
2025-03-23 20:23:57 +0200

    Deleted Matrix library.

--- src/Graph/Models/Symmetrize.ts
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import type { GraphEdgeType, GraphNodeType, GraphType } from "../../../Types";
-
-export const Symmetrize = (
-  G: GraphType,
-  type: "average" | "input first" | "output first",
-) => {
-};
