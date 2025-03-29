0a8cab8eace7cd36f2b05c389734eee5ab3531d7
euriklis <exel_mmm@abv.bg>
2025-03-30 00:25:37 +0200

    Added type NodeType

--- Types/index.ts
+++ Types/index.ts
@@ -3,3 +3,12 @@ export type GraphDataType = null;
 export type AbstractAttributesType = {
   [property: string]: unknown;
 };
+
+export type NodeType = {
+  id: string | number;
+  name: string;
+  data: {
+    value: number | string | number[] | number[][] | string[] | string[][];
+    [property: string]: unknown;
+  };
+};
