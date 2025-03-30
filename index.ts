3e0dfccf28d08057fe12f89cca0ec98ef56a113d
euriklis <exel_mmm@abv.bg>
2025-03-30 16:30:14 +0300

    Added tests

--- index.ts
+++ index.ts
@@ -1,14 +1,43 @@
 "use strict";
-import { BST } from "./src";
+import { v4 as uuid } from "uuid";
+import { BST, AVLTree, DoublyLinkedList, Queue } from "./src";
 
-const bst = new BST();
+const avl = new AVLTree<{
+  name: string;
+  family: string;
+  id?: string | number;
+}>();
 
-bst.insert({ name: "Allah" }, "Allah");
-bst.insertMany([{ name: "Aaaakbar" }, { name: "Mohamed" }, { name: "Kerim" }]);
+const isEmpty = avl.isEmpty;
 
-console.log(bst.size, bst.height());
-const a = [...bst];
+console.log(isEmpty);
 
-bst.print();
+avl.insert({ name: "Nevena", family: "Coneva", id: "Nevena Coneva" });
+avl.insert({
+  name: "Vanya",
+  family: "Karastoycheva",
+  id: "Vanya Karastoycheva",
+});
 
-bst.loop((n) => (console.log(`Hello from ${n.data.name}`), true));
+avl.insertMany([
+  { name: "Strahil", family: "Penkov", id: "Strahil Penkov" },
+  { name: "Chocho", family: "Popyordanov", id: "Chocho Popyordanov" },
+  { name: "Chochka", family: "Popyordanova", id: "Chochhka Popyordanova" },
+  { name: "Velislav", family: "Karastoychev", id: "Velislav Karastoychev" },
+  { name: "Albena", family: "Kyurtekova", id: "Albena Kyurtekova" },
+  { name: "Bojidar", family: "Stoev", id: "Bojidar Stoev" },
+]);
+for (let i: number = 0; i < 20; i++) {
+  avl.insert({ name: uuid(), family: uuid(), id: uuid() });
+}
+
+const dll = new DoublyLinkedList<{ name: string; family: string }>();
+
+const isDLLEmpty = dll.isEmpty;
+console.log(isDLLEmpty);
+dll.addLast({ name: "Gyuro", family: "Mihailov" }, "GM");
+dll.insertAfter("GM", { name: "Velislav", family: "Karastoychev" });
+const isVeliHere = dll.any((n) => n.name === "Velislav");
+const isMilkoHere = dll.any((n) => n.name === "Mihail");
+console.log(isVeliHere);
+console.log(isMilkoHere);
