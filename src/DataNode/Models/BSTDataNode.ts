227a47a55f596725f1b226a3fce3382c39f90ce3
euriklis <exel_mmm@abv.bg>
2025-04-01 10:57:01 +0300

    Updated to generic type

--- src/DataNode/Models/BSTDataNode.ts
+++ src/DataNode/Models/BSTDataNode.ts
@@ -5,81 +5,81 @@ import { DataNode } from "./DataNode";
  * Class representing a binary search tree (BST) data node.
  * Inherits from the DataNode class.
  */
-export class BSTDataNode extends DataNode {
+export class BSTDataNode<T = unknown> extends DataNode<T> {
   /**
    * Reference to the parent BST data node.
    * @protected
-   * @type {BSTDataNode | null}
+   * @type {this | null}
    */
-  protected _prev: BSTDataNode | null = null;
+  protected _prev: this | null = null;
 
   /**
    * Reference to the right child BST data node.
    * @protected
-   * @type {BSTDataNode | null}
+   * @type {this | null}
    */
-  protected _right: BSTDataNode | null = null;
+  protected _right: this | null = null;
 
   /**
    * Reference to the left child BST data node.
    * @protected
-   * @type {BSTDataNode | null}
+   * @type {this | null}
    */
-  protected _left: BSTDataNode | null = null;
+  protected _left: this | null = null;
 
   /**
    * Creates an instance of BSTDataNode.
-   * @param {*} data - Data to associate with the node.
+   * @param {T} data - Data to associate with the node.
    */
-  constructor(data: any) {
+  constructor(data: T) {
     super(data);
   }
 
   /**
    * Gets the reference to the parent BST data node.
-   * @returns {BSTDataNode | null} - The parent BST data node.
+   * @returns {this | null} - The parent BST data node.
    */
-  get prev(): BSTDataNode | null {
+  get prev(): this | null {
     return this._prev;
   }
 
   /**
    * Sets the reference to the parent BST data node.
-   * @param {BSTDataNode | null} node - The parent BST data node.
+   * @param {this | null} node - The parent BST data node.
    */
-  set prev(node: BSTDataNode | null) {
+  set prev(node: this | null) {
     this._prev = node;
   }
 
   /**
    * Gets the reference to the right child BST data node.
-   * @returns {BSTDataNode | null} - The right child BST data node.
+   * @returns {this | null} - The right child BST data node.
    */
-  get right(): BSTDataNode | null {
+  get right(): this | null {
     return this._right || null;
   }
 
   /**
    * Sets the reference to the right child BST data node.
-   * @param {BSTDataNode | null} node - The right child BST data node.
+   * @param {this | null} node - The right child BST data node.
    */
-  set right(node: BSTDataNode | null) {
+  set right(node: this | null) {
     this._right = node || null;
   }
 
   /**
    * Gets the reference to the left child BST data node.
-   * @returns {BSTDataNode | null} - The left child BST data node.
+   * @returns {this | null} - The left child BST data node.
    */
-  get left(): BSTDataNode | null {
+  get left(): this | null {
     return this._left || null;
   }
 
   /**
    * Sets the reference to the left child BST data node.
-   * @param {BSTDataNode | null} node - The left child BST data node.
+   * @param {this | null} node - The left child BST data node.
    */
-  set left(node: BSTDataNode | null) {
+  set left(node: this | null) {
     this._left = node || null;
   }
 }
