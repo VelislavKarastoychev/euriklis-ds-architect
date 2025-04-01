1fb118be32d035bc42ad13ada903025b14c6e410
euriklis <exel_mmm@abv.bg>
2025-04-01 10:53:11 +0300

    Added types to the variables.

--- src/AVL/Models/SetBalanceFactors.ts
+++ src/AVL/Models/SetBalanceFactors.ts
@@ -3,19 +3,21 @@
 import type { AVLTree } from "..";
 import type { AVLDataNode } from "../../DataNode";
 
-export const SetBalanceFactorsForward = (
+export const SetBalanceFactorsForward = <T>(
   node: AVLDataNode | null,
-  tree: AVLTree,
+  tree: AVLTree<T>,
 ): void => {
   if (!node) return;
-  node.balance = tree.height(node.right) - tree.height(node.left);
+  node.balance =
+    tree.height(node.right as AVLDataNode<T>) -
+    tree.height(node.left as AVLDataNode<T>);
   SetBalanceFactorsForward(node.left, tree);
   SetBalanceFactorsForward(node.right, tree);
 };
 
-export const SetBalanceFactorsAfterDeletion = (
+export const SetBalanceFactorsAfterDeletion = <T>(
   node: AVLDataNode | null,
-  tree: AVLTree,
+  tree: AVLTree<T>,
 ): void => {
   if (!node) return;
   SetBalanceFactorsForward(node, tree);
@@ -23,39 +25,42 @@ export const SetBalanceFactorsAfterDeletion = (
   return SetBalanceFactorsAfterDeletion(node.prev, tree);
 };
 
-export const UpdateNodeBalance = (node: AVLDataNode, tree: AVLTree) => {
+export const UpdateNodeBalance = <T>(
+  node: AVLDataNode<T>,
+  tree: AVLTree<T>,
+) => {
   if (node.balance === -2) {
-    if (((node.left as AVLDataNode)?.balance) < 0) {
+    if ((node.left as AVLDataNode<T>)?.balance < 0) {
       tree.singleRightRotation(node);
-      SetBalanceFactorsForward(node, tree);
-    } else if (((node.left as AVLDataNode)?.balance) > 0) {
+      SetBalanceFactorsForward<T>(node, tree);
+    } else if ((node.left as AVLDataNode<T>)?.balance > 0) {
       tree.doubleLeftRightRotation(node);
-      SetBalanceFactorsForward(node, tree);
+      SetBalanceFactorsForward<T>(node, tree);
     }
   } else if (node.balance === 2) {
-    if (((node.right as AVLDataNode)?.balance) > 0) {
+    if ((node.right as AVLDataNode<T>)?.balance > 0) {
       tree.singleLeftRotation(node);
-      SetBalanceFactorsForward(node, tree);
+      SetBalanceFactorsForward<T>(node, tree);
     }
-    if (((node.right as AVLDataNode)?.balance) < 0) {
+    if ((node.right as AVLDataNode<T>)?.balance < 0) {
       tree.doubleRightLeftRotation(node);
-      SetBalanceFactorsForward(node, tree);
+      SetBalanceFactorsForward<T>(node, tree);
     }
   }
 };
 
-export const SetBalanceFactorsBackward = (
-  node: AVLDataNode,
-  tree: AVLTree,
+export const SetBalanceFactorsBackward = <T>(
+  node: AVLDataNode<T>,
+  tree: AVLTree<T>,
   rebalancing: boolean = true,
 ): void => {
   if (node.prev) {
     if (node === node.prev.left) node.prev.balance -= 1;
     else node.prev.balance += 1;
-    node = node.prev as AVLDataNode;
+    node = node.prev as AVLDataNode<T>;
     if (!node.balance) return;
     if (rebalancing) UpdateNodeBalance(node, tree);
 
-    return SetBalanceFactorsBackward(node, tree);
+    return SetBalanceFactorsBackward<T>(node, tree);
   }
 };
