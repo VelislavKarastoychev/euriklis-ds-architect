1b123e5ab4b52e8e566de12f4a2654fe5fd74894
euriklis <exel_mmm@abv.bg>
2025-04-04 08:58:12 +0300

    Added final test for the AVL trees.

--- index.ts
+++ index.ts
@@ -4,7 +4,10 @@ import { AVLTree } from "./src";
 
 const avl = new AVLTree<{ name: string; description: string }>();
 
-avl.insert({ name: "Union", description: "A transport company." });
+avl.insert({
+  name: "Union",
+  description: "A transport company.",
+});
 avl.insert({ name: "Arda Tour", description: "A bus company." });
 
 avl.print(undefined, undefined, undefined, (node) => node.data?.description);
