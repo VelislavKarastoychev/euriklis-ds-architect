cf59bd48eff11e1057d84f9966b0d4b56eb4aeac
euriklis <exel_mmm@abv.bg>
2025-04-04 08:58:51 +0300

    Fixed types in order to be generic type.

--- src/BST/Models/BSTLoop.ts
+++ src/BST/Models/BSTLoop.ts
@@ -6,13 +6,15 @@ import type { Queue } from "../../Queue";
 
 export const BSTLoop = <T extends any, AbstractBSTDataNode extends BSTDataNode>(
   callback: (node: AbstractBSTDataNode, bst: BST<T>) => boolean,
-  queue: Queue,
+  queue: Queue<AbstractBSTDataNode | null>,
   tree: BST<T>,
 ): void => {
   if (queue.isEmpty) return;
-  const node: AbstractBSTDataNode = queue.dequeue();
-  if (!callback(node, tree)) return;
-  if (node.right) queue.enqueue(node.right);
-  if (node.left) queue.enqueue(node.left);
+  const node: AbstractBSTDataNode | null = queue.dequeue();
+  if (!callback(node as AbstractBSTDataNode, tree)) return;
+  if ((node as AbstractBSTDataNode).right)
+    queue.enqueue((node as AbstractBSTDataNode).right);
+  if ((node as AbstractBSTDataNode).left)
+    queue.enqueue((node as AbstractBSTDataNode).left);
   return BSTLoop(callback, queue, tree);
 };
