595010f1322cb0f9225036914e92d9c4f3e66a5a
euriklis <exel_mmm@abv.bg>
2025-04-12 21:31:05 +0300

    Added GraphDataNode.

--- src/DataNode/Models/index.ts
+++ src/DataNode/Models/index.ts
@@ -4,8 +4,13 @@ export { AVLDataNode } from "./AVLDataNode";
 export { BSTDataNode } from "./BSTDataNode";
 export { ComputationGraphDataNode } from "./ComputationGraphDataNode";
 export { DataNode } from "./DataNode";
-export { GraphDataEdge } from "./GraphDataEdgeOld";
+export {
+  BaseGraphDataNode,
+  DirectedGraphDataNode,
+  UndirectedGraphDataNode,
+} from "./GraphDataNode.ts";
+export { GraphDataEdge } from "./GraphDataEdge.ts";
 export { GraphDataElement } from "./GraphDataElement";
-export { GraphDataNode } from "./GraphDataNodeOld";
+// export { GraphDataNode } from "./GraphDataNodeOld";
 export { HeapDataNode } from "./HeapDataNode";
 export { LinkedDataNode } from "./LinkedDataNode";
