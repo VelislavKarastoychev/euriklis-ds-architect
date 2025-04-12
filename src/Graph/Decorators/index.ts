2438ed1430cbedcb2c90d51a84aaf124cd720a37
euriklis <exel_mmm@abv.bg>
2025-04-12 21:28:10 +0300

    Added new decorators.

---
+++ src/Graph/Decorators/index.ts
@@ -0,0 +1,4 @@
+"use strict";
+
+export { ifNodeExistsThrow } from "./ifNodeExistsThrow.decorator";
+export { ifSourceOrTargetNodeNotExistThrow } from "./ifSourceOrTargetNodesNotExistThrow.decorator";
