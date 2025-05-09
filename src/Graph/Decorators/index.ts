10ac0f351e325ec9489d1ca6a2a43f7ff044fc04
euriklis <exel_mmm@abv.bg>
2025-05-09 18:31:23 +0300

    Exports of all the decorators.

--- src/Graph/Decorators/index.ts
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-export { ifNodeExistsThrow } from "./ifNodeExistsThrow.decorator";
-export { ifSourceOrTargetNodeNotExistThrow } from "./ifSourceOrTargetNodesNotExistThrow.decorator";
