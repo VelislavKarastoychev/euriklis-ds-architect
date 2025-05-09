d319a74738cab6807b287baeb98a1beb8f5a8794
euriklis <exel_mmm@abv.bg>
2025-05-09 18:31:38 +0300

    Implemented error messages for the Graph library.

--- src/Graph/Errors/errorMessages.ts
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-export const NodeAlreadyExistsTxt = (name: string): string =>
-  `Node ${name} already exists in the Graph.`;
-export const NodeNotExistsTxt = (name: string): string =>
-  `Node ${name} does not exist in the graph.`;
