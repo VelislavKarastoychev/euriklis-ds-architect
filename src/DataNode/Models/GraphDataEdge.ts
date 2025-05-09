c0e28fceda0e1cd5aceafcb817586b6da63548df
euriklis <exel_mmm@abv.bg>
2025-05-09 18:30:23 +0300

    New implementation of the GraphDataEdge.

--- src/DataNode/Models/GraphDataEdge.ts
+++ src/DataNode/Models/GraphDataEdge.ts
@@ -1,64 +1,96 @@
 "use strict";
+import { v4 as uuid } from "uuid";
+import { GraphDataNode, Node, Vertex } from "./GraphDataNode";
 
-import { DataNode } from "./DataNode";
-import {
-  DirectedGraphDataNode,
-  UndirectedGraphDataNode,
-} from "./GraphDataNode";
+export abstract class GraphDataEdge<
+  S extends GraphDataNode<any, any>,
+  T extends GraphDataNode<any, any>,
+  D = unknown,
+> {
+  private __ID__: string = "";
+  protected __SOURCE__: S | null = null;
+  protected __TARGET__: T | null = null;
+  protected __DATA__: D | null = null;
 
-export class GraphDataEdge<
-  INData = unknown,
-  NData = unknown,
-  EData = unknown,
-> extends DataNode<EData> {
-  protected _link?:
-    | DirectedGraphDataNode<NData, INData, EData>
-    | UndirectedGraphDataNode<NData, INData, EData>;
-  protected __weight__: number = 1;
+  constructor({ source, target, data }: { source: S; target: T; data?: D }) {
+    this.id = uuid();
+    this.source = source;
+    this.target = target;
+    if (typeof data !== "undefined") this.data = data;
+  }
+
+  get id(): string {
+    return this.__ID__;
+  }
+
+  set id(id: string) {
+    this.__ID__ = uuid();
+  }
+  get source(): S | null {
+    return this.__SOURCE__;
+  }
+
+  set source(s: S) {
+    this.__SOURCE__ = s;
+  }
+
+  get target(): T | null {
+    return this.__TARGET__;
+  }
+
+  set target(t: T) {
+    this.__TARGET__ = t;
+  }
+
+  get data(): D | null {
+    return this.__DATA__;
+  }
+
+  set data(d: D) {
+    this.__DATA__ = d;
+  }
+}
+
+export class Edge<D = unknown> extends GraphDataEdge<
+  Vertex<any>,
+  Vertex<any>,
+  D
+> {
   constructor({
-    link,
+    source,
+    target,
     data,
-    weight = 1,
   }: {
-    link:
-      | DirectedGraphDataNode<NData, INData, EData>
-      | UndirectedGraphDataNode<NData, INData, EData>;
-    data: EData;
-    weight: number;
+    source: Vertex<any>;
+    target: Vertex<any>;
+    data: D;
   }) {
-    super(data);
-    this.link = link;
-    this.__weight__ = weight;
+    super({ source, target, data });
   }
+}
 
-  get link():
-    | DirectedGraphDataNode<NData, INData, EData>
-    | UndirectedGraphDataNode<NData, INData, EData>
-    | undefined {
-    return this._link;
-  }
-
-  set link(
-    link:
-      | DirectedGraphDataNode<NData, INData, EData>
-      | UndirectedGraphDataNode<NData, INData, EData>,
-  ) {
-    this._link = link;
-  }
-
-  get data(): EData | null {
-    return this._data;
-  }
-
-  set data(d: EData) {
-    this._data = d;
+export class Arc<D = unknown> extends GraphDataEdge<Node<any>, Node<any>, D> {
+  protected __WEIGHT__: number = 1;
+  constructor({
+    source,
+    target,
+    data,
+    weight,
+  }: {
+    source: Node<any>;
+    target: Node<any>;
+    data: D;
+    weight?: number;
+  }) {
+    super({ source, target, data });
+    if (typeof weight !== "undefined") this.weight = weight;
   }
 
   get weight(): number {
-    return this.__weight__;
+    return this.__WEIGHT__;
   }
 
-  set wight(w: number) {
-    this.__weight__ = w;
+  set weight(w: number) {
+    this.__WEIGHT__ = w;
   }
 }
