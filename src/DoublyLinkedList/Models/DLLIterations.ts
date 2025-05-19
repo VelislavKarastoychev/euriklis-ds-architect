8058ce2640568510cf4c84f247f84b0af8bb78d6
euriklis <exel_mmm@abv.bg>
2025-05-19 11:29:35 +0300

    Added generic types.

--- src/DoublyLinkedList/Models/DLLIterations.ts
+++ src/DoublyLinkedList/Models/DLLIterations.ts
@@ -3,27 +3,23 @@
 import type { DoublyLinkedList } from "..";
 import type { LinkedDataNode } from "../../DataNode";
 
-export const DLLTraverse = (
-  callback: (d: any, id: string, list: DoublyLinkedList) => void,
-  node: LinkedDataNode | null,
-  list: DoublyLinkedList,
+export const DLLTraverse = <T>(
+  callback: (d: any, id: string, list: DoublyLinkedList<T>) => void,
+  node: LinkedDataNode<T> | null,
+  list: DoublyLinkedList<T>,
   inversed: boolean,
 ): void => {
   if (!node) return;
   callback(node.data, node.id, list);
   node = inversed ? node.prev : node.next;
-  
+
   return DLLTraverse(callback, node, list, inversed);
 };
 
-export const DLLLoop = (
-  callback: (
-    data: any,
-    id: string,
-    list: DoublyLinkedList,
-  ) => boolean,
-  node: LinkedDataNode | null,
-  list: DoublyLinkedList,
+export const DLLLoop = <T>(
+  callback: (data: T | null, id: string, list: DoublyLinkedList<T>) => boolean,
+  node: LinkedDataNode<T> | null,
+  list: DoublyLinkedList<T>,
   inversed: boolean,
 ): void => {
   if (!node) return;
