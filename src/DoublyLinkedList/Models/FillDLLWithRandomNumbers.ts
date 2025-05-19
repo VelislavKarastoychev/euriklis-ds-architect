dafe783c9d4a7fa638a604b6c925d20760345903
euriklis <exel_mmm@abv.bg>
2025-05-19 11:29:42 +0300

    Added generic types.

--- src/DoublyLinkedList/Models/FillDLLWithRandomNumbers.ts
+++ src/DoublyLinkedList/Models/FillDLLWithRandomNumbers.ts
@@ -4,15 +4,15 @@ import type { DoublyLinkedList } from "..";
 import type { Integer } from "../../../Types";
 
 export const FillDLLWithRandomNumbers = (
-  l: DoublyLinkedList,
+  l: DoublyLinkedList<number>,
   n: Integer,
   from: number,
   to: number,
   seed: number,
-  callback?: (d: number, id: string, list: DoublyLinkedList) => any,
-) => {
+  callback?: (d: number, id: string, list: DoublyLinkedList<number>) => any,
+): void => {
   let j: Integer, k: number, d: number;
-  for (j = n; j-- > 1;) {
+  for (j = n; j-- > 1; ) {
     seed <<= 0;
     k = (seed / 127773) >> 0;
     seed = (16807 * (seed - k * 127773) - k * 2836) >> 0;
@@ -20,7 +20,7 @@ export const FillDLLWithRandomNumbers = (
     d = from + (to - from) * seed * 4.656612875e-10;
     if (callback) l.addLast(callback(d, `${n - j - 1}`, l));
     else l.addLast(from + (to - from) * seed * 4.656612875e-10, `${n - j - 1}`);
-    j--, seed <<= 0;
+    j--, (seed <<= 0);
     k = (seed / 127773) >> 0;
     seed = (16807 * (seed - k * 127773) - k * 2836) >> 0;
     if (seed < 0) seed += 2147483647;
