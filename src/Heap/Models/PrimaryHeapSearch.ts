707d8481748ead91ad73b98b31c3723669b27b72
euriklis <exel_mmm@abv.bg>
2025-06-06 23:17:09 +0300

    Reimplemented PrimaryHeapSearch function.

--- src/Heap/Models/PrimaryHeapSearch.ts
+++ src/Heap/Models/PrimaryHeapSearch.ts
@@ -3,34 +3,20 @@
 import type { HeapType, Integer } from "../../../Types";
 import type { HeapDataNode } from "../../DataNode";
 
-const PrimaryHeapSearchIterator = (
-  heap: HeapDataNode[],
-  id: string,
-  index: Integer,
-  stopCondition: string,
-  found: HeapDataNode[]
-) => Function(
-    "heap",
-    "id",
-    "index",
-    `
-    const PrimaryHeapSearchRecursion = (heap, id, index) => {
-      const node = heap[index];
-      if (!node) return;
-      if (node.id === id) found.push(node);
-      if (${stopCondition}) return;
-      else PrimaryHeapSearchRecursion(heap, id, index << 1 + 1);
-      PrimaryHeapSearchRecursion(heap, id, index + 1)
-    }
-    `
-  )(heap, id, index);
-
 export const PrimaryHeapSearch = (
   heap: HeapDataNode[],
   id: string,
   type: HeapType,
-) => {
+): HeapDataNode[] | null => {
   const found: HeapDataNode[] = [];
-  const stopCondition = type === "max" ? `node.id < id`: `heap[index].id > id`;
-  return PrimaryHeapSearchIterator(heap, id, 0, stopCondition, found);
+  const search = (index: Integer): void => {
+    if (index >= heap.length) return;
+    const node = heap[index];
+    if (node.id === id) found.push(node);
+    if (type === "max" ? node.id < id : node.id > id) return;
+    search((index << 1) + 1);
+    search((index << 1) + 2);
+  };
+  search(0);
+  return found.length ? found : null;
 };
