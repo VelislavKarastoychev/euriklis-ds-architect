1c2a97067586651d8a26406d39631b5727a69c3e
euriklis <exel_mmm@abv.bg>
2025-06-06 23:17:29 +0300

    Reimplemented ShiftDownHeapify and shiftDown.

--- src/Heap/Models/ShiftDownHeapify.ts
+++ src/Heap/Models/ShiftDownHeapify.ts
@@ -1,75 +1,45 @@
 "use strict";
 
 import type { Integer } from "../../../Types";
+import type { HeapDataNode } from "../../DataNode";
 
-type T = NonNullable<any>;
-const ShiftDown = (
-  data: T[],
-  i: Integer,
-  comparison: string,
-  chooseMIndex: string,
-  n: Integer,
-  callback: (a: T, b: T, i: Integer, j: Integer, heap: T[]) => 1 | -1 | 0,
-): void =>
-  Function(
-    "heap",
-    "i",
-    "n",
-    "callback",
-    `
-    const shiftDownRecursion = (heap, i) => { 
-      const left = (i << 1) + 1;
-      const right = (i + 1) << 1;
-      if (left >= n) return;
-      let m = right;
-      if (m >= n) m = left;
-      else if (${chooseMIndex} < 0) m = left;
-      if (${comparison} > 0) {
-        [heap[m], heap[i]] = [heap[i], heap[m]];
-      }
-      
-      return shiftDownRecursion(heap, m, n);
-      
-    }
-    return shiftDownRecursion(heap, i, n);
-    `,
-  )(data, i, n, callback);
-
-const PrimaryShiftDownIterator = (
-  data: T[],
-  chooseMIndex: string,
-  comparison: string,
-  n: Integer,
-  m: Integer,
-  callback: (a: T, b: T, i: Integer, j: Integer, data: T[]) => 1 | -1 | 0,
+const shiftDown = (
+  heap: HeapDataNode[],
+  index: Integer,
+  size: Integer,
+  type: "max" | "min",
 ): void => {
-  if (m < 0) return;
-  ShiftDown(data, m, comparison, chooseMIndex, n, callback);
-  return PrimaryShiftDownIterator(
-    data,
-    chooseMIndex,
-    comparison,
-    n,
-    m - 1,
-    callback,
-  );
+  while (true) {
+    const left = (index << 1) + 1;
+    const right = left + 1;
+    let target = index;
+    if (
+      left < size &&
+      (type === "max"
+        ? heap[left].id > heap[target].id
+        : heap[left].id < heap[target].id)
+    ) {
+      target = left;
+    }
+    if (
+      right < size &&
+      (type === "max"
+        ? heap[right].id > heap[target].id
+        : heap[right].id < heap[target].id)
+    ) {
+      target = right;
+    }
+    if (target === index) break;
+    [heap[index], heap[target]] = [heap[target], heap[index]];
+    index = target;
+  }
 };
 
 export const PrimaryShiftDownHeapify = (
-  data: T[],
-  callback: (a: T, b: T, i: Integer, j: Integer, heap: T[]) => 1 | -1 | 0,
-) => {
-  const n = data.length;
-  const m = (n - 2) >> 1;
-  if (n < 2) return;
-  const chooseMIndex = "callback(heap[right], heap[left], right, left, heap)";
-  const comparison = "callback(heap[m], heap[i], m, i, heap)";
-  return PrimaryShiftDownIterator(
-    data,
-    chooseMIndex,
-    comparison,
-    n,
-    m,
-    callback,
-  );
+  data: HeapDataNode[],
+  type: "max" | "min",
+): void => {
+  for (let i = (data.length - 2) >> 1; i >= 0; i--) {
+    shiftDown(data, i, data.length, type);
+  }
 };
