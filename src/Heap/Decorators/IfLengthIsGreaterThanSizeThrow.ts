256a4d64c29e18ca2e119db7141817bbe04e0bbd
euriklis <exel_mmm@abv.bg>
2025-06-06 23:16:54 +0300

    Reimplemented decorator for the Heap.

--- src/Heap/Decorators/IfLengthIsGreaterThanSizeThrow.ts
+++ src/Heap/Decorators/IfLengthIsGreaterThanSizeThrow.ts
@@ -3,15 +3,18 @@
 import type { PrimaryHeap } from "..";
 
 export function ifLengthIsGreaterThanSizeThrow(
-  error: Function,
+  error: () => never,
 ): (_: any, __: string, descriptor: PropertyDescriptor) => void {
   return function (_: any, __: string, descriptor: PropertyDescriptor): void {
     const method = descriptor.value;
 
     descriptor.value = function (data: any) {
       if (
-        data && ((this as PrimaryHeap).length >= (this as PrimaryHeap).size)
-      ) error();
+        typeof data !== "undefined" &&
+        (this as PrimaryHeap).length >= (this as PrimaryHeap).size
+      ) {
+        error();
+      }
       return method.call(this as PrimaryHeap, data);
     };
   };
