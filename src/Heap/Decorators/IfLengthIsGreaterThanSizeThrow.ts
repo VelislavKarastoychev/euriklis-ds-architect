cf0c3858e91d60b5dfa03e6d875033e0c83cd061
euriklis <exel_mmm@abv.bg>
2025-06-07 17:10:42 +0300

    Added types in order to be generic

--- src/Heap/Decorators/IfLengthIsGreaterThanSizeThrow.ts
+++ src/Heap/Decorators/IfLengthIsGreaterThanSizeThrow.ts
@@ -2,20 +2,20 @@
 
 import type { PrimaryHeap } from "..";
 
-export function ifLengthIsGreaterThanSizeThrow(
+export function ifLengthIsGreaterThanSizeThrow<T = unknown>(
   error: () => never,
 ): (_: any, __: string, descriptor: PropertyDescriptor) => void {
   return function (_: any, __: string, descriptor: PropertyDescriptor): void {
     const method = descriptor.value;
 
-    descriptor.value = function (data: any) {
+    descriptor.value = function (data: T) {
       if (
         typeof data !== "undefined" &&
-        (this as PrimaryHeap).length >= (this as PrimaryHeap).size
+        (this as PrimaryHeap<T>).length >= (this as PrimaryHeap<T>).size
       ) {
         error();
       }
-      return method.call(this as PrimaryHeap, data);
+      return method.call(this as PrimaryHeap<T>, data);
     };
   };
 }
