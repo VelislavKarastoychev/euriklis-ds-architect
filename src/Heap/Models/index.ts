7ddc768aabdb38ac97f19efdac1bf4afeb5bfd4e
euriklis <exel_mmm@abv.bg>
2025-06-08 22:11:41 +0300

    Exports the PrimaryHeapSearchIndex.

--- src/Heap/Models/index.ts
+++ src/Heap/Models/index.ts
@@ -3,3 +3,4 @@
 export { PrimaryShiftUp } from "./ShiftUp";
 export { PrimaryShiftDownHeapify } from "./ShiftDownHeapify";
 export { PrimaryHeapSearch } from "./PrimaryHeapSearch";
+export { PrimaryHeapSearchIndex } from "./PrimaryHeapSearchIndex";
