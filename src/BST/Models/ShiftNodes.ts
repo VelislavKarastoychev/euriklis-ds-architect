e0be61516cd2568c222d414c7d22c35ecb83c592
euriklis <exel_mmm@abv.bg>
2025-06-15 12:02:17 +0300

    Fixed type of AbstractBSTDataNode.

--- src/BST/Models/ShiftNodes.ts
+++ src/BST/Models/ShiftNodes.ts
@@ -4,14 +4,14 @@ import type { BST } from "..";
 import type { BSTDataNode } from "../../DataNode";
 
 export const ShiftNodes = <
-  T extends any,
-  AbstractBSTDataNode extends BSTDataNode,
+  T extends unknown,
+  AbstractBSTDataNode extends BSTDataNode<T>,
 >(
   tree: BST<T>,
   u: AbstractBSTDataNode,
   v: AbstractBSTDataNode | null,
 ) => {
-  if (!u?.prev) tree.root = v;
+  if (!u?.prev) (tree.root as AbstractBSTDataNode | null) = v;
   else if (u === u.prev.left) u.prev.left = v;
   else u.prev.right = v;
   if (v) v.prev = u.prev;
