2b5fe5b11f4b5572d297ba7b326c2e7195815f7f
euriklis <exel_mmm@abv.bg>
2025-06-15 10:56:47 +0300

    Deleted ComputationGraphDataNode.

--- src/DataNode/Models/index.ts
+++ src/DataNode/Models/index.ts
@@ -2,11 +2,9 @@
 
 export { AVLDataNode } from "./AVLDataNode";
 export { BSTDataNode } from "./BSTDataNode";
-export { ComputationGraphDataNode } from "./ComputationGraphDataNode";
 export { DataNode } from "./DataNode";
 export { GraphDataNode, Node, Vertex } from "./GraphDataNode.ts";
 export { GraphDataEdge, Edge, Arc } from "./GraphDataEdge.ts";
 export { GraphDataElement } from "./GraphDataElement";
-// export { GraphDataNode } from "./GraphDataNodeOld";
 export { HeapDataNode } from "./HeapDataNode";
 export { LinkedDataNode } from "./LinkedDataNode";
