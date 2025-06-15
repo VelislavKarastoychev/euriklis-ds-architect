bf25836d8f5272cda52ff58dbb2e1825128c7ba2
euriklis <exel_mmm@abv.bg>
2025-06-15 14:42:15 +0300

    Added StateGraph class.

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,4 @@ export { DoublyLinkedList } from "./DoublyLinkedList";
 export { BST } from "./BST";
 export { Heap, PrimaryHeap } from "./Heap";
 export { Queue } from "./Queue";
-export { Graph, BaseGraph, BaseNetwork } from "./Graph";
+export { Graph, BaseGraph, BaseNetwork, StateGraph } from "./Graph";
