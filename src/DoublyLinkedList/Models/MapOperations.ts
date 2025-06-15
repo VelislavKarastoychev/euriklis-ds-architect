1a24f9e26cbddd42778e0bb3e5abd4d619555815
euriklis <exel_mmm@abv.bg>
2025-06-15 12:03:39 +0300

    Fixed types.

--- src/DoublyLinkedList/Models/MapOperations.ts
+++ src/DoublyLinkedList/Models/MapOperations.ts
@@ -3,24 +3,24 @@
 import { AVLTree } from "../../AVL";
 import type { LinkedDataNode } from "../../DataNode";
 
-export const MapInsertion = (
-  Node: LinkedDataNode,
-  map: null | Map<string, LinkedDataNode> | AVLTree,
+export const MapInsertion = <T = undefined>(
+  Node: LinkedDataNode<T>,
+  map: null | Map<string, LinkedDataNode<T>> | AVLTree<LinkedDataNode<T>>,
 ): void => {
   if (map instanceof Map) map.set(Node.id, Node);
   if (map instanceof AVLTree) map.insert(Node, Node.id);
 };
 
-export const MapDeletion = (
+export const MapDeletion = <T = unknown>(
   id: string,
-  map: null | Map<string, LinkedDataNode> | AVLTree,
+  map: null | Map<string, LinkedDataNode> | AVLTree<LinkedDataNode<T>>,
 ): void => {
-  if (map) map.delete(id);
+  if (map) map.delete(id as any);
 };
 
-export const MapHas = (
+export const MapHas = <T = unknown>(
   id: string,
-  map: Map<string, LinkedDataNode> | AVLTree,
+  map: Map<string, LinkedDataNode> | AVLTree<LinkedDataNode<T>>,
 ) => {
   if (map) return map.has(id);
   else return false;
