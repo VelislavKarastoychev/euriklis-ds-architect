4cde15bdf06a54ada3232beb4672a8df94fb9fdc
euriklis <exel_mmm@abv.bg>
2025-06-15 12:03:16 +0300

    Fixed types and transformed the function to be generic

--- src/DoublyLinkedList/Models/FilterDLL.ts
+++ src/DoublyLinkedList/Models/FilterDLL.ts
@@ -3,16 +3,16 @@
 import type { DoublyLinkedList } from "..";
 import type { LinkedDataNode } from "../../DataNode";
 
-export const FilterDLL = (
-  node: LinkedDataNode | null,
-  callback: (d: any, id?: string, list?: DoublyLinkedList) => boolean,
-  l1: DoublyLinkedList,
-  l2: DoublyLinkedList,
+export const FilterDLL = <T = undefined>(
+  node: LinkedDataNode<T> | null,
+  callback: (d: any, id?: string, list?: DoublyLinkedList<T>) => boolean,
+  l1: DoublyLinkedList<T>,
+  l2: DoublyLinkedList<T>,
   inversed: boolean,
 ): void => {
   if (!node) return;
   const { data, id } = node;
-  if (callback(data, id, l1)) l2.addLast(data, id);
+  if (callback(data, id, l1)) l2.addLast(data as T, id);
   node = inversed ? node.prev : node.next;
 
   return FilterDLL(node, callback, l1, l2, inversed);
