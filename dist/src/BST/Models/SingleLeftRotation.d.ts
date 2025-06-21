e32eed8307c8aceb6e62cb1242d6a197fcd9e64d
euriklis <exel_mmm@abv.bg>
2025-06-21 22:08:54 +0300

    Added types

---
+++ dist/src/BST/Models/SingleLeftRotation.d.ts
@@ -0,0 +1,3 @@
+import type { BST } from "..";
+import type { BSTDataNode } from "../../DataNode";
+export declare const SingleLeftRotation: <T extends any, AbstractBSTDataNode extends BSTDataNode<T>>(a: AbstractBSTDataNode, tree: BST<T>) => void;
