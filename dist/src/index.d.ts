6980169fd8698bb598b282daca3fa8760788408a
euriklis <exel_mmm@abv.bg>
2025-06-21 22:11:47 +0300

    Added types

---
+++ dist/src/index.d.ts
@@ -0,0 +1,7 @@
+export { AVLTree } from "./AVL";
+export { DynamicStack, StaticStack } from "./Stack";
+export { DoublyLinkedList } from "./DoublyLinkedList";
+export { BST } from "./BST";
+export { Heap, PrimaryHeap } from "./Heap";
+export { Queue } from "./Queue";
+export { Graph, BaseGraph, BaseNetwork, StateGraph } from "./Graph";
