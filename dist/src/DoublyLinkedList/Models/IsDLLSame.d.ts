922474bde29591609f102e5c5842655dc3b8c5d1
euriklis <exel_mmm@abv.bg>
2025-06-21 22:10:33 +0300

    Added types

---
+++ dist/src/DoublyLinkedList/Models/IsDLLSame.d.ts
@@ -0,0 +1,3 @@
+import type { LinkedDataNode } from "../../DataNode";
+export declare const IsDLLSame: (n1: LinkedDataNode | null, n2: LinkedDataNode | null) => boolean;
+export declare const IsDLLExactlySame: (n1: LinkedDataNode | null, n2: LinkedDataNode | null) => boolean;
