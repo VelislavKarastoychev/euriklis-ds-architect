4397340cd33e0922bc4ae380e9bcc98dd97526ad
euriklis <exel_mmm@abv.bg>
2025-06-21 22:10:06 +0300

    Added types

---
+++ dist/src/DataNode/Models/index.d.ts
@@ -0,0 +1,8 @@
+export { AVLDataNode } from "./AVLDataNode";
+export { BSTDataNode } from "./BSTDataNode";
+export { DataNode } from "./DataNode";
+export { GraphDataNode, Node, Vertex } from "./GraphDataNode.ts";
+export { GraphDataEdge, Edge, Arc } from "./GraphDataEdge.ts";
+export { GraphDataElement } from "./GraphDataElement";
+export { HeapDataNode } from "./HeapDataNode";
+export { LinkedDataNode } from "./LinkedDataNode";
