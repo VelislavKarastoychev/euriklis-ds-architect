33461ed137f83620344d9b2208b1c1748ca1bda4
euriklis <exel_mmm@abv.bg>
2025-06-21 22:08:12 +0300

    Added types

---
+++ dist/src/BST/Models/Backward.d.ts
@@ -0,0 +1,3 @@
+import type { BSTDataNode } from "../../DataNode";
+export declare const RightBackward: <AbstractBSTDataNode extends BSTDataNode>(x: AbstractBSTDataNode | null) => AbstractBSTDataNode | null;
+export declare const LeftBackward: <AbstractBSTDataNode extends BSTDataNode>(x: AbstractBSTDataNode | null) => AbstractBSTDataNode | null;
