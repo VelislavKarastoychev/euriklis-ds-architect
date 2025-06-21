0dd83fe28541b0accb99cf5b7a0049f3a855911b
euriklis <exel_mmm@abv.bg>
2025-06-21 22:08:47 +0300

    Added types

---
+++ dist/src/BST/Models/OrderCallback.d.ts
@@ -0,0 +1,9 @@
+import type { BSTNodeComparisonCallbackType, BSTNodeValueComparisonCallbackType } from "../../../Types";
+/**
+ * Compares two BST nodes based on their IDs.
+ */
+export declare const CompareNodes: BSTNodeComparisonCallbackType;
+/**
+ * Compares a BST node with a given value based on the node's ID.
+ */
+export declare const CompareNodeWithValue: BSTNodeValueComparisonCallbackType;
