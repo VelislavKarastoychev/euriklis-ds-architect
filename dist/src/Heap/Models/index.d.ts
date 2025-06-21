174278bd9cb69a5f5837c9c31962e420124ccf8b
euriklis <exel_mmm@abv.bg>
2025-06-21 22:11:31 +0300

    Added types

---
+++ dist/src/Heap/Models/index.d.ts
@@ -0,0 +1,4 @@
+export { PrimaryShiftUp } from "./ShiftUp";
+export { PrimaryShiftDownHeapify } from "./ShiftDownHeapify";
+export { PrimaryHeapSearch } from "./PrimaryHeapSearch";
+export { PrimaryHeapSearchIndex } from "./PrimaryHeapSearchIndex";
