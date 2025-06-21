cbee6e0eff493bb988d722693a0eb9b1b407237c
euriklis <exel_mmm@abv.bg>
2025-06-21 22:10:43 +0300

    Added types

---
+++ dist/src/DoublyLinkedList/Models/index.d.ts
@@ -0,0 +1,7 @@
+export { GenerateMap } from "./GenerateMap";
+export { MapDeletion, MapHas, MapInsertion } from "./MapOperations";
+export { DLLLoop, DLLTraverse } from "./DLLIterations";
+export { FindNodeFromDLL, FindNodeFromMap } from "./FindNodeById";
+export { FillDLLWithRandomNumbers } from "./FillDLLWithRandomNumbers";
+export { FilterDLL } from "./FilterDLL";
+export { IsDLLExactlySame, IsDLLSame } from "./IsDLLSame";
