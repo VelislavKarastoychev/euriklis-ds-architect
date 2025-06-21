7e697fce301ea3ff9a71e10a8c1ec537a187ed1f
euriklis <exel_mmm@abv.bg>
2025-06-21 22:11:50 +0300

    Added types

---
+++ dist/src/utils/index.d.ts
@@ -0,0 +1,4 @@
+export declare const max: (...values: number[]) => number;
+export declare const errorGenerator: (name: string) => (text: string) => never;
+export declare const IsSame: (a: any, b: any) => boolean;
+export declare const IsIntegerArray: (item: any) => boolean;
