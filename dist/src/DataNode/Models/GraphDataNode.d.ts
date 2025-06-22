5977a3de9724a43a134c8634266b99dd058e06ec
euriklis <exel_mmm@abv.bg>
2025-06-22 22:41:53 +0300

    Added description for some classes.

--- dist/src/DataNode/Models/GraphDataNode.d.ts
+++ dist/src/DataNode/Models/GraphDataNode.d.ts
@@ -1,4 +1,8 @@
 import { GraphDataEdge, Edge, Arc } from "./GraphDataEdge";
+/**
+ * Base class for all graph-related nodes. Holds incoming and outgoing
+ * edges as well as basic metadata like name and data payload.
+ */
 export declare abstract class GraphDataNode<O = unknown, E extends GraphDataEdge<GraphDataNode<any, E>, GraphDataNode<any, E>, unknown> = GraphDataEdge<GraphDataNode<any, any>, GraphDataNode<any, any>, unknown>> {
     private __ID__;
     protected __NAME__: string;
@@ -37,9 +41,15 @@ export declare abstract class GraphDataNode<O = unknown, E extends GraphDataEdge
     get inDegree(): number;
     get outDegree(): number;
 }
+/**
+ * Basic graph vertex implementation holding outgoing and incoming edges.
+ */
 export declare class Vertex<D = unknown> extends GraphDataNode<D, Edge<any>> {
     protected createEdge<T = unknown>(target: Vertex<any>, data: T): Edge<T>;
 }
+/**
+ * Vertex variant storing a numeric value used by weighted graphs.
+ */
 export declare class Node<D = unknown> extends GraphDataNode<D, Arc<any>> {
     protected __VALUE__: number;
     constructor({ name, data, value }: {
