5cccfac5d3415a61e8017f2f0c8000f759a6b0ff
euriklis <exel_mmm@abv.bg>
2025-06-22 22:42:02 +0300

    Added description for some classes.

--- dist/src/DataNode/Models/HeapDataNode.d.ts
+++ dist/src/DataNode/Models/HeapDataNode.d.ts
@@ -1,4 +1,7 @@
 import { DataNode } from "./DataNode";
+/**
+ * Simple wrapper around `DataNode` used by heap structures.
+ */
 export declare class HeapDataNode<T = unknown> extends DataNode<T> {
     constructor(data: T);
 }
