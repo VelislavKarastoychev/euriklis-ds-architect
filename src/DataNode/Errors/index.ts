6c2c000f63dceed158a6e86d51984df26bf95f1a
euriklis <exel_mmm@abv.bg>
2025-07-26 19:43:00 +0300

    Updated errors

--- src/DataNode/Errors/index.ts
+++ src/DataNode/Errors/index.ts
@@ -4,9 +4,8 @@ import { errorGenerator } from "../../utils";
 
 const runError = errorGenerator("@euriklis/graphworlds Graph node error");
 
-export const EdgeAlreadyExists: (link: string) => () => never = (
-  link: string,
-): never => runError(`The edge ${link} already exists.`);
+export const EdgeAlreadyExists = (link: string): never =>
+  runError(`The edge ${link} already exists.`);
 
-export const NameIsRequired = (): (() => never) =>
+export const NameIsRequired = (): never =>
   runError("The name is required in order to create a node.");
